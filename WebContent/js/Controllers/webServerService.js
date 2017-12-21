6b647843d96c2c5bfdd9461901128d06d4ae7731
senzitgit <34739394+senzitgit@users.noreply.github.com>
2017-12-21 23:23:30 +0530

    Version 2.0

---
+++ WebContent/js/Controllers/webServerService.js
@@ -0,0 +1,4 @@
+var webServerUrl = "http://192.168.0.110:8080/ClazPortal/AppServerService.jsp";
+var changePicUrl = "http://192.168.0.110:8080/ClazPortal/ChangePictureService.jsp";
+var libraryUploadUrl = "http://192.168.0.110:8080/ClazPortal/UploadFileService.jsp";
+var webSocketIp="192.168.0.110:8080";
